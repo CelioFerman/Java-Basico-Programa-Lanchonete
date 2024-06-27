d59291a9cc78c160c1933e44d4eea025211b52ca
Celio Fermano <celiolucio94@hotmail.com>
2024-06-27 10:22:41 -0300

    Get-Set
    Criando os metodos get and set para pegar e criar novos atributos.

---
+++ src/Escola/Escola.java
@@ -0,0 +1,12 @@
+package Escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe Junior");
+        felipe.setIdade(11);
+
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos. ");
+
+    }
+}
